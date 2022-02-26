5ce4bc8e7f2ef15df0d4ca584c4fe9853c17f156
fraser148 <fjrennie1@outlook.com>
2022-02-26 16:39:05 +0000

    Authentication

---
+++ app/frontend/src/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const Dashboard = () => {
+    return (
+        <div className="main">
+            <NavBar/>
+            <div className="content">
+                Hi there jo
+            </div>
+        </div>
+        
+    )
+}
+
+export default Dashboard
